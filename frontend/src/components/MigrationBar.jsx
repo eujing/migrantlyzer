7e11519bfdcf8e9265c0a7ee60c07655944523eb
Gideon Lee <Gideon Lee>
2017-04-29 21:47:11 +0800

    implemented topfactorslist and migrationbar

--- frontend/src/components/MigrationBar.jsx
+++ frontend/src/components/MigrationBar.jsx
@@ -1,9 +1,31 @@
 import React from "react"
 
 export default class MigrationBar extends React.Component {
+
     render() {
         return (
-            <p>MigrationBar</p>
+            <div>
+	            <p>MigrationBar</p>
+	            	<div>
+	              		<div>
+	              			{this.props.origin}
+	              		</div>
+	              		<div>
+	              			{this.props.emigration}
+	              		</div>
+	            	</div>
+	            	<div>
+	            		<span> arrow </span>
+	            	</div>
+	            	<div>
+	            		<div>
+	            			{this.props.destination}
+	            		</div>
+	            		<div>
+	            			{this.props.immigration}
+	            		</div>
+	            	</div>
+            </div>
         )
     }
 }
--- frontend/src/components/TopFactorsList.jsx
+++ frontend/src/components/TopFactorsList.jsx
@@ -2,13 +2,111 @@ import React from "react"
 import Factor from "./Factor.jsx"
 
 export default class TopFactorsList extends React.Component {
+
+	extractFactorWeights(originData, destinationData, year) {
+		//primitively takes the difference in rank for that particular year
+		var factorsWeights = {};
+		var factorsList = Object.keys(destinationData);
+		factorsList.forEach(function(value, index) {
+			var weight = destinationData[value][year]["rank"] - originData[value][year]["rank"];
+			factorsWeights[value] = weight;			
+			return 0;
+		})
+		return factorsWeights;
+	}
+
+	extractTopPull(originData, destinationData, factorWeights, year) {
+		//top pull for that year only
+		var cur;
+		var topFactor;
+		var next;
+		for (var factor in factorWeights) {
+			if (!cur) {
+				cur = factorWeights[factor];
+				topFactor = factor
+			} else {
+				next = factorWeights[factor];
+				if (next > cur) {
+					cur = next;
+					topFactor = factor;
+				}
+			}
+		};
+		return topFactor;
+	}
+
+	extractTopNumberOfPulls(originData, destinationData, factorWeights, year, number) {
+		var topPulls = [];
+		var internalOriginData = Object.assign({}, originData);
+		var internalDestinationData = Object.assign({}, destinationData);
+		var internalFactorWeights = Object.assign({}, factorWeights);
+		var curTop;
+		for (var i = 0; i < number; i++ ){
+			curTop = this.extractTopPull(internalOriginData, internalDestinationData, internalFactorWeights, year);
+			topPulls.push(curTop);
+			delete internalOriginData[curTop];
+			delete internalDestinationData[curTop];
+			delete internalFactorWeights[curTop];
+		}
+		return topPulls;
+	}
+
+	extractTopPush(originData, destinationData, factorWeights, year) {
+		//top push for that year only
+		var cur;
+		var worstFactor;
+		var next;
+		for (var factor in factorWeights) {
+			if (!cur) {
+				cur = factorWeights[factor];
+				worstFactor = factor
+			} else {
+				next = factorWeights[factor];
+				if (next < cur) {
+					cur = next;
+					worstFactor = factor;
+				}
+			}
+		};
+		return worstFactor;
+	}
+
+	extractTopNumberOfPushes(originData, destinationData, factorWeights, year, number) {
+		var topPushes = [];
+		var internalOriginData = Object.assign({}, originData);
+		var internalDestinationData = Object.assign({}, destinationData);
+		var internalFactorWeights = Object.assign({}, factorWeights);
+		var curTop;
+		for (var i = 0; i < number; i++ ){
+			curTop = this.extractTopPush(internalOriginData, internalDestinationData, internalFactorWeights, year);
+			topPushes.push(curTop);
+			delete internalOriginData[curTop];
+			delete internalDestinationData[curTop];
+			delete internalFactorWeights[curTop];
+		}
+		return topPushes;
+	}
+
+
     render() {
+    	var indexData = this.props.exclusiveIndexData;
+    	var origin = this.props.origin;
+    	var destination = this.props.destination;
+    	var year = 2015;
+
+    	var factorWeights = this.extractFactorWeights(indexData[origin], indexData[destination], year);
+    	var topPulls = this.extractTopNumberOfPulls(indexData[origin], indexData[destination], factorWeights, year, 2);
+    	var topPushes = this.extractTopNumberOfPushes(indexData[origin], indexData[destination], factorWeights, year, 2);
+
         return (
             <div>
                 <p>TopFactorsList</p>
+                <div>top two factors: {topPulls}</div>
+                <div>top two factors: {topPushes}</div>
                 <ul>
                     <Factor />
                 </ul>
             </div>)
     }
 }
+
